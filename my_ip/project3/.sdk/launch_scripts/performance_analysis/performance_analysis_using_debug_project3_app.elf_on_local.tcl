a35da2aa5d8ad9500f8b56b5616e44a0857df562
Yuusuke <y38y38@gmai.com>
2020-04-29 20:57:48 +0900

    add sdk file

---
+++ my_ip/project3/.sdk/launch_scripts/performance_analysis/performance_analysis_using_debug_project3_app.elf_on_local.tcl
@@ -0,0 +1,15 @@
+connect -url tcp:127.0.0.1:3121
+source C:/Xilinx/SDK/2018.2/scripts/sdk/util/zynqmp_utils.tcl
+targets -set -nocase -filter {name =~"APU*" && jtag_cable_name =~ "Avnet USB-to-JTAG/UART Pod V1 1234-oj1A"} -index 1
+loadhw -hw C:/Users/t/sdk/project3/hw_platform_0/system.hdf -mem-ranges [list {0x80000000 0xbfffffff} {0x400000000 0x5ffffffff} {0x1000000000 0x7fffffffff}]
+configparams force-mem-access 1
+targets -set -nocase -filter {name =~"APU*" && jtag_cable_name =~ "Avnet USB-to-JTAG/UART Pod V1 1234-oj1A"} -index 1
+source C:/Users/t/sdk/project3/hw_platform_0/psu_init.tcl
+psu_init
+catch {psu_protection}
+targets -set -nocase -filter {name =~"*A53*0" && jtag_cable_name =~ "Avnet USB-to-JTAG/UART Pod V1 1234-oj1A"} -index 1
+rst -processor
+dow C:/Users/t/sdk/project3/project3_app/Debug/project3_app.elf
+configparams force-mem-access 0
+targets -set -nocase -filter {name =~"*A53*0" && jtag_cable_name =~ "Avnet USB-to-JTAG/UART Pod V1 1234-oj1A"} -index 1
+con
