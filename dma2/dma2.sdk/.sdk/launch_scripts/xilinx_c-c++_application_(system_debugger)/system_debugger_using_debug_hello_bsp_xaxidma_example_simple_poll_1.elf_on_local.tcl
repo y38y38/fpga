3eeda2d28eee2f0fd244ec92fd6ac09d8dcf7030
Yuusuke <y38y38@gmai.com>
2020-05-29 00:30:28 +0900

    add adma dma

---
+++ dma2/dma2.sdk/.sdk/launch_scripts/xilinx_c-c++_application_(system_debugger)/system_debugger_using_debug_hello_bsp_xaxidma_example_simple_poll_1.elf_on_local.tcl
@@ -0,0 +1,19 @@
+connect -url tcp:127.0.0.1:3121
+source /tools/Xilinx/SDK/2019.1/scripts/sdk/util/zynqmp_utils.tcl
+targets -set -nocase -filter {name =~"APU*" && jtag_cable_name =~ "Avnet USB-to-JTAG/UART Pod V1 1234-oj1A"} -index 1
+loadhw -hw /home/y/fpga/dma2/dma2.sdk/design_1_wrapper_hw_platform_0/system.hdf -mem-ranges [list {0x80000000 0xbfffffff} {0x400000000 0x5ffffffff} {0x1000000000 0x7fffffffff}]
+configparams force-mem-access 1
+targets -set -nocase -filter {name =~"APU*" && jtag_cable_name =~ "Avnet USB-to-JTAG/UART Pod V1 1234-oj1A"} -index 1
+source /home/y/fpga/dma2/dma2.sdk/design_1_wrapper_hw_platform_0/psu_init.tcl
+psu_init
+after 1000
+psu_ps_pl_isolation_removal
+after 1000
+psu_ps_pl_reset_config
+catch {psu_protection}
+targets -set -nocase -filter {name =~"*A53*0" && jtag_cable_name =~ "Avnet USB-to-JTAG/UART Pod V1 1234-oj1A"} -index 1
+rst -processor
+dow /home/y/fpga/dma2/dma2.sdk/hello_bsp_xaxidma_example_simple_poll_1/Debug/hello_bsp_xaxidma_example_simple_poll_1.elf
+configparams force-mem-access 0
+targets -set -nocase -filter {name =~"*A53*0" && jtag_cable_name =~ "Avnet USB-to-JTAG/UART Pod V1 1234-oj1A"} -index 1
+con
