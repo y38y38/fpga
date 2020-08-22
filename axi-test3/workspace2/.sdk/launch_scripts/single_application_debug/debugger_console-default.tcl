c4177166e870a928117104e48d458e66138d88e2
Yuusuke <y38y38@gmail.com>
2020-08-22 18:53:40 +0900

    add freertos console

---
+++ axi-test3/workspace2/.sdk/launch_scripts/single_application_debug/debugger_console-default.tcl
@@ -0,0 +1,24 @@
+connect -url tcp:127.0.0.1:3121
+source /tools/Xilinx/Vitis/2020.1/scripts/vitis/util/zynqmp_utils.tcl
+targets -set -nocase -filter {name =~"APU*"}
+rst -system
+after 3000
+targets -set -filter {jtag_cable_name =~ "Avnet USB-to-JTAG/UART Pod V1 1234-oj1A" && level==0 && jtag_device_ctx=="jsn-USB-to-JTAG/UART Pod V1-1234-oj1A-14710093-0"}
+fpga -file /home/y/fpga/axi-test3/workspace2/console/_ide/bitstream/design_1_wrapper.bit
+targets -set -nocase -filter {name =~"APU*"}
+loadhw -hw /home/y/fpga/axi-test3/workspace2/design_1_wrapper/export/design_1_wrapper/hw/design_1_wrapper.xsa -mem-ranges [list {0x80000000 0xbfffffff} {0x400000000 0x5ffffffff} {0x1000000000 0x7fffffffff}] -regs
+configparams force-mem-access 1
+targets -set -nocase -filter {name =~"APU*"}
+set mode [expr [mrd -value 0xFF5E0200] & 0xf]
+targets -set -nocase -filter {name =~ "*A53*#0"}
+rst -processor
+dow /home/y/fpga/axi-test3/workspace2/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/boot/fsbl.elf
+set bp_17_57_fsbl_bp [bpadd -addr &XFsbl_Exit]
+con -block -timeout 60
+bpremove $bp_17_57_fsbl_bp
+targets -set -nocase -filter {name =~ "*A53*#0"}
+rst -processor
+dow /home/y/fpga/axi-test3/workspace2/console/Debug/console.elf
+configparams force-mem-access 0
+targets -set -nocase -filter {name =~ "*A53*#0"}
+con
